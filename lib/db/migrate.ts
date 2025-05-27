da20a926ad0a7c9db6e0172fa6d6fecc81e6fa0d
sh-sho <sho.obata.c5@gmail.com>
2025-05-27 13:54:59 +0000

    test

--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -6,18 +6,18 @@ import postgres from 'postgres';
 // config({
 //   path: '.env.local',
 // });
-// let env = config();
-// console.log('🌍 Loaded environment variabless:', env);
+let env = config();
+console.log('🌍 Loaded environment variabless:', env);
 
 const runMigrate = async () => {
   console.log('🌍 All environment variables:', process.env);
   // if (!process.env.POSTGRES_URL) {
-  if (!process.env.POSTGRESQLCONNSTR_POSTGRES_URL) {
+  if (!process.env.APPSETTING_POSTGRES_URL) {
     throw new Error('POSTGRES_URL is not defined');
   }
 
   // const connection = postgres(process.env.POSTGRES_URL, { max: 1 });
-  const connection = postgres(process.env.POSTGRESQLCONNSTR_POSTGRES_URL, { max: 1 });
+  const connection = postgres(process.env.APPSETTING_POSTGRES_URL, { max: 1 });
   const db = drizzle(connection);
 
   console.log('⏳ Running migrations...');
