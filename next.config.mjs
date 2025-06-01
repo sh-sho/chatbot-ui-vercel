83ad14c4e489e9b3de0733db854984fb180ffeff
sh-sho <sho.obata.c5@gmail.com>
2025-06-01 16:09:41 +0000

    edit: yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "standalone",
-  output: "export",
+  output: "standalone",
+  // output: "export",
   images: {
     domains: ["cdn.builder.io"],
   },
